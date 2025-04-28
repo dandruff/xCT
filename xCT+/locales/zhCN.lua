66a345edba3a31995345364145858833e86cec42
RedAces <redaces@gmx.de>
2025-04-28 19:09:14 +0200

    add header to zhCN file

--- xCT+/locales/zhCN.lua
+++ xCT+/locales/zhCN.lua
@@ -1,5 +1,24 @@
+--[[   ____    ______
+      /\  _`\ /\__  _\   __
+ __  _\ \ \/\_\/_/\ \/ /_\ \___
+/\ \/'\\ \ \/_/_ \ \ \/\___  __\
+\/>  </ \ \ \L\ \ \ \ \/__/\_\_/
+ /\_/\_\ \ \____/  \ \_\  \/_/
+ \//\/_/  \/___/    \/_/
+
+ [=====================================]
+ [  Author: Feylynn-Antonidas EU       ]
+ [  xCT+ Version 4.x.x                 ]
+ [  ©2010-2025 All Rights Reserved.    ]
+ [====================================]]
+
+-- Translators:
+-- * https://github.com/fredako
+
 local AddonName = ...
 
+-- 3rd param: isDefault
+-- 4th param: silent
 local L = LibStub:GetLibrary("AceLocale-3.0"):NewLocale(AddonName, "zhCN", false, false)
 if not L then return end
 
